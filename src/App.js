58271dafe590014786f9562080593fe73f104b2d
Steelman9876 <130379153+Steelman9876@users.noreply.github.com>
2024-08-27 23:22:31 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Layout from './components/Layout';
+import './styles/global.css';
+
+function App() {
+  return (
+    <Layout>
+      {/* Your main content goes here */}
+    </Layout>
+  );
+}
+
+export default App;
